edd632ca46ffa4ca31877ae8e128de61812c0cd1
John Tringham <john@tringham.co.uk>
2024-07-25 12:44:15 +0100

    Deployment autocommit - 25/07/2024 12:44:14.61

--- deploy.bat
+++ deploy.bat
@@ -1,4 +1,8 @@
-echo off
+
+git status
+
+set /p DUMMY=Hit ENTER to deploy...
+
 echo doing git stuff now:
 echo "Going to deploy now"
 hugo & echo john.seedganggames.com > docs/CNAME & git add -A & git commit -m "Deployment autocommit - %date% %time%" & git push
